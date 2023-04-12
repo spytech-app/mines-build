28e742c0f81ba00a1ab9ff662db9639cdbbca45e
spytech-app <spytechapp@gmail.com>
2023-04-12 19:16:34 -0300

    feat: first deploy

---
+++ precache-manifest.a138eff4c0b634f3d1cdd4e644ec6666.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3c90424aaf669cb0b2afb30ac901c85",
+    "url": "/spytech-app/minestruque-build/index.html"
+  },
+  {
+    "revision": "aeeda0eedcf185869ae7",
+    "url": "/spytech-app/minestruque-build/static/css/main.eba759ca.chunk.css"
+  },
+  {
+    "revision": "e7a606c75aaa32996ac9",
+    "url": "/spytech-app/minestruque-build/static/js/2.9cabe443.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/spytech-app/minestruque-build/static/js/2.9cabe443.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aeeda0eedcf185869ae7",
+    "url": "/spytech-app/minestruque-build/static/js/main.ce1f871e.chunk.js"
+  },
+  {
+    "revision": "b34bc3a770cab913fb9f",
+    "url": "/spytech-app/minestruque-build/static/js/runtime-main.cca78213.js"
+  },
+  {
+    "revision": "a90dfb3914de0910ac87c739e91d3398",
+    "url": "/spytech-app/minestruque-build/static/media/background.a90dfb39.png"
+  },
+  {
+    "revision": "60e802b3d27d1c049c25aa6d2b8684d1",
+    "url": "/spytech-app/minestruque-build/static/media/bomb.60e802b3.png"
+  },
+  {
+    "revision": "18a5a7fd7e91a180b377901254f991b3",
+    "url": "/spytech-app/minestruque-build/static/media/diamond.18a5a7fd.png"
+  },
+  {
+    "revision": "914a52f50539ed7cdd1af6561d8b8b65",
+    "url": "/spytech-app/minestruque-build/static/media/disabled.914a52f5.png"
+  },
+  {
+    "revision": "914a52f50539ed7cdd1af6561d8b8b65",
+    "url": "/spytech-app/minestruque-build/static/media/logo.914a52f5.png"
+  }
+]);
